7dfb5bae76a24ca5890b898ec5fc146e71d18400
User <_email@gmail.com>
2026-03-20 16:03:08 +0300

    initial project setup

---
+++ mini-ums/src/main/java/com/ums/mini_ums/MiniUmsApplication.java
@@ -0,0 +1,13 @@
+package com.ums.mini_ums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniUmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniUmsApplication.class, args);
+	}
+
+}
---
+++ mini-ums/src/test/java/com/ums/mini_ums/MiniUmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ums.mini_ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniUmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
